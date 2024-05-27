da77320fd60441172ff65069da3286935e00fc9c
Adarsh Singh <143992541+adarsh-singh01@users.noreply.github.com>
2024-05-27 13:27:24 +0530

    Update AboutUs.jsx fix position

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -24,7 +24,7 @@ const AboutUs = () => {
                     <img src={adarsh} className="w-24 h-22 rounded-full" alt="Adarsh Singh" />
                     <div className="ml-4">
                         <h3 className="text-xl font-semibold">Adarsh Singh</h3>
-                        <p className="text-gray-700">Co-founder</p>
+                        <p className="text-gray-700">Founder</p>
                     </div>
                 </div>
                 <div className="flex items-center">
