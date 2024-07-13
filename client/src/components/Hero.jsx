97c0062386f1cecb0e07113c52946200f071ec75
Ramakrushna Biswal <125277258+RamakrushnaBiswal@users.noreply.github.com>
2024-07-13 12:28:30 +0530

    Home page animation added (#166)

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import giffy from "../assets/Hero.gif";
 import { Link } from "react-router-dom";
-
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function Hero() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  })
   return (
-
     <div>
       <div className="hero m-3 flex justify-center items-center space-x-2 my-12 md:my-16">
 
-        <div className="card bg-gray-200 max-w-sm rounded-lg p-6 space-y-4 md:space-y-8">
+        <div className="card bg-gray-200 max-w-sm rounded-lg p-6 space-y-4 md:space-y-8" data-aos="fade-right">
           <div className="heading  font-medium font-Rubik text-2xl md:text-4xl">
             Wanna reduce your <span className="text-green-500">carbon footprints</span>? <span className="">Calculate</span> it first 
           </div>
@@ -22,7 +28,7 @@ function Hero() {
           </div>
         </div>
         <div className="gif">
-          <img className="rounded-lg hidden sm:block md:max-w-sm" src={giffy} />
+          <img className="rounded-lg hidden sm:block md:max-w-sm" src={giffy} data-aos="fade-left"/>
         </div>
       </div>
       
