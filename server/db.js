03c2d5db35008bcf9c1e54a1fe09a17b0765b47b
Adarsh Singh <143992541+adarsh-singh01@users.noreply.github.com>
2024-08-06 23:54:21 +0530

    Update db.js connection thru con string

--- server/db.js
+++ server/db.js
@@ -4,12 +4,12 @@ import env from "dotenv";
 env.config();
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
+  /*user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  //connectionString:process.env.CON_STRING,
+  port: process.env.PG_PORT,*/
+  connectionString:process.env.CON_STRING,
   ssl:false
   /*ssl: {
     rejectUnauthorized: false, 
