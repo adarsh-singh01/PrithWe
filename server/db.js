7e482cafb86d7473321776d89994eb378043d7a1
Adarsh Singh <143992541+adarsh-singh01@users.noreply.github.com>
2024-08-18 11:02:16 +0530

    Update db.js error handling

--- server/db.js
+++ server/db.js
@@ -9,6 +9,40 @@ const db = new pg.Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: process.env.PG_CERTIFICATE,
+  },
+});
+
+// Connect to the database
+db.connect()
+  .then(() => console.log("Connected to the database successfully"))
+  .catch((err) => console.error('Connection error:', err.stack));
+
+// Function to perform a query
+const query = async (text, params) => {
+  try {
+    const result = await db.query(text, params);
+    return result;
+  } catch (error) {
+    console.error('Error executing query:', error);
+  }
+};
+
+export { query };
+{/*
+import pg from 'pg';
+import env from "dotenv";
+
+env.config();
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
   //connectionString:process.env.CON_STRING,
   //ssl:false
   ssl: {
@@ -32,3 +66,4 @@ const query = async (text, params) => {
 };
 
 export { query };
+*/}
