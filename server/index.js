22c3dd6a4e124e15f72032a719c77549d55c5b46
Adarsh Singh <143992541+adarsh-singh01@users.noreply.github.com>
2024-10-15 16:28:29 +0530

    fix render spin down Update index.js

--- server/index.js
+++ server/index.js
@@ -198,6 +198,23 @@ if (process.env.NODE_ENV === "production") {
 
 
 
+const url = `https://prithwe.onrender.com/`; 
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+
+setInterval(reloadWebsite, interval);
+
+
 
 
 
